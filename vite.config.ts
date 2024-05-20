949ca25310243b6322daa912e42ff00979cf094b
Gregory Croisdale <gregtc@umich.edu>
2024-05-20 11:08:41 -0400

    ready to be a component

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    })
+  ],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        popout: './popout.html',
-      },
+    lib: {
+      entry: './src/index.ts', // Single entry point for the library
+      name: 'df-coding',
+      fileName: (format) => `df-coding.${format}.js`,
+      formats: ['es', 'cjs'] // Specify the formats you want to build
     },
-  },
+    rollupOptions: {
+      // Ensure to externalize dependencies that shouldn't be bundled
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 });
